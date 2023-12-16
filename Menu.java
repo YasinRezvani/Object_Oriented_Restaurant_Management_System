73115835a1e766191dd6708005791975f5817851
Yasin Rezvani <yasinrezvani2025@gmail.com>
2023-12-16 23:21:24 +0330

    add methods(add & remove food or drink) to menu Menu.java

--- Menu.java
+++ Menu.java
@@ -7,3 +7,35 @@ public class Menu {
     private List<Food> foods;
     private List<Drink> drinks;
 }
+
+
+    public Menu() {
+        this.foods = new ArrayList<>();
+        this.drinks = new ArrayList<>();
+    }
+
+    public void addFood(Food food) {
+        foods.add(food);
+    }
+
+    public void removeFood(Food food) {
+        foods.remove(food);
+    }
+
+    public void addDrink(Drink drink) {
+        drinks.add(drink);
+    }
+
+    public void removeDrink(Drink drink) {
+        drinks.remove(drink);
+    }
+
+    public List<Food> getFoods() {
+        return foods;
+    }
+
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+}
+
