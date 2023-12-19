408e359e766d854b716501c45a8964cd6dcafa71
Yasin Rezvani <yasinrezvani2025@gmail.com>
2023-12-19 16:05:54 +0330

    implements of getter for private fields

--- Food.java
+++ Food.java
@@ -25,3 +25,28 @@ public class Food {
         this.price = price;
         this.mealTime = mealTime;
     }
+
+        public Type getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public MealTime getMealTime() {
+        return mealTime;
+    }
+
+    public static int getTotalSales() {
+        return totalSales;
+    }
+
+    public static void incrementTotalSales() {
+        totalSales++;
+    }
+}
