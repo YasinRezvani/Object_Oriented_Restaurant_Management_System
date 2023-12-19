13ed08fb2d6a0501a31efbbb8365869f5c1307ba
Yasin Rezvani <yasinrezvani2025@gmail.com>
2023-12-19 16:00:38 +0330

    create enum for mealtime and type of foods , create constructor

--- Food.java
+++ Food.java
@@ -1,1 +1,27 @@
+package restaurant;
 
+public class Food {
+    public enum Type {
+        FAST_FOOD,
+        IRANIAN_FOOD,
+        CHINESE_FOOD
+    }
+
+    public enum MealTime {
+        BREACK_FAST,
+        LUNCH,
+        DINNER
+    }
+
+    private Type type;
+    private String name;
+    private double price;
+    private MealTime mealTime;
+    private static int totalSales;
+
+    public Food(Type type, String name, double price, MealTime mealTime) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+        this.mealTime = mealTime;
+    }
