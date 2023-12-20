d672840840e8002328b59e3142be86bcc20cec1f
Yasin Rezvani <yasinrezvani2025@gmail.com>
2023-12-20 16:56:16 +0330

    add getter and setter for order to place a order of customer Order.java

--- Order.java
+++ Order.java
@@ -1,1 +1,52 @@
+package restaurant;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private List<Food> foods;
+    private List<Drink> drinks;
+
+    public Order() {
+        this.foods = new ArrayList<>();
+        this.drinks = new ArrayList<>();
+    }
+
+    public Order(Food food, Drink drink) {
+        this();
+        addFood(food);
+        addDrink(drink);
+    }
+
+    public void addFood(Food food) {
+        foods.add(food);
+    }
+
+    public void addDrink(Drink drink) {
+        drinks.add(drink);
+    }
+
+    public List<Food> getFoods() {
+        return foods;
+    }
+
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+    
+    public double getPrice() {
+        double total = 0;
+
+        
+        for (Food food : foods) {
+            total += food.getPrice();
+        }
+
+        
+        for (Drink drink : drinks) {
+            total += drink.getPrice();
+        }
+
+        return total;
+    }
+}
