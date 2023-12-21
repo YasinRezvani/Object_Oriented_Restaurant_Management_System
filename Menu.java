c49a8a5f979f46ad6b726eec660ef9c5988099d3
Yasin Rezvani <yasinrezvani2025@gmail.com>
2023-12-21 16:54:45 +0330

    add useful comments Menu.java

--- Menu.java
+++ Menu.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
+    // Determine list to store any food or drink
     private List<Food> foods;
     private List<Drink> drinks;
 }
 
-
+    // Constructor Menu() class
     public Menu() {
         this.foods = new ArrayList<>();
         this.drinks = new ArrayList<>();
     }
 
+    // Method to add food to List Food
     public void addFood(Food food) {
         foods.add(food);
     }
@@ -21,7 +23,7 @@ public class Menu {
     public void removeFood(Food food) {
         foods.remove(food);
     }
-
+    // Method to add food to List Drink
     public void addDrink(Drink drink) {
         drinks.add(drink);
     }
@@ -29,11 +31,12 @@ public class Menu {
     public void removeDrink(Drink drink) {
         drinks.remove(drink);
     }
-
+    // Getter for any arraylist on List Food
     public List<Food> getFoods() {
         return foods;
     }
 
+    // Getter for any arraylist on List Drink
     public List<Drink> getDrinks() {
         return drinks;
     }
