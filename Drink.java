445d970f2bda86e7cb5b964d2fcff1aeacaeca0a
Yasin Rezvani <yasinrezvani2025@gmail.com>
2023-12-21 18:09:33 +0330

    add comments Drink.java

--- Drink.java
+++ Drink.java
@@ -2,21 +2,25 @@
 package restaurant;
 
 public class Drink {
+    // Define enum for type to choose easily
     public enum Type {
         HOT_DRINK,
         ICE_DRINK
     }
 
+    // Define private fields
     private Type type;
     private String name;
     private double price;
     private static int totalSales;
 
+    // Define constructor
     public Drink(Type type, String name, double price) {
         this.type = type;
         this.name = name;
         this.price = price;
     }
+    // Getters and setters for private fields
     public Type getType() {
         return type;
     }
